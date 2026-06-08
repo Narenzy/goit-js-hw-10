4d50eea0e3e0a1144dcf084a2df8d00c41e2995b
Narenzy <mahankotaras@gmail.com>
2026-06-08 14:29:14 +0000

    Deploying to gh-pages from @ Narenzy/goit-js-hw-10@bb97594303c35bf3c1ae09cbd1590724fe4499fd 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                              */import"./assets/vendor-BbbuE1sJ.js";const t=document.querySelector(".btn");t.addEventListener("click",()=>{});
+//# sourceMappingURL=2-snackbar.js.map
